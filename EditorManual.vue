01c67cd8c3e70dfa667f3d15a8ba31667a1887d5
pedroporto29 <pedro-porto@hotmail.com.br>
2020-02-13 09:32:05 -0300

    jodit

---
+++ EditorManual.vue
@@ -0,0 +1,41 @@
+<template>
+  <b-container fluid>
+    <b-row>
+      <b-col>
+        <b-form-textarea
+          id="textarea"
+          v-model="text"
+          placeholder="Enter something..."
+          rows="100"
+          max-rows="6"
+        ></b-form-textarea>
+      </b-col>
+
+      <b-col class="sticky-top">
+        <textarea id="editor" name="editor"></textarea>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+  export default {
+    name: 'EditorManual',
+    data () {
+      return {
+          editor: ''
+      }
+    },
+    mounted(){
+        this.editor = new this.$jodit('#editor', {
+          height: 400,
+          uploader: {
+            insertImageAsBase64URI: true
+          },
+          buttons: "source,|,underline,italic,eraser,|,,ul,ol,,|,fontsize,,image,table,link,|,align,undo,redo,,fullsize,print,selectall",
+          askBeforePasteHTML: false,
+          askBeforePasteFromWord: false,
+        });
+    }
+  }
+</script>
---
+++ main.js
@@ -0,0 +1,25 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import jodit from 'jodit'
+
+Object.defineProperty(Vue.prototype, '$jodit', { value: jodit });
+
+Vue.use(BootstrapVue)
+Vue.use(BootstrapVueIcons)
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
